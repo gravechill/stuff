5d080811e54e61262d84c9818a3acab5b9d6726e
shommey <bgmisa@msn.com>
2018-04-03 01:45:27 +0200

    Added side menu

--- MyCryptoStats/js/test3.js
+++ MyCryptoStats/js/test3.js
@@ -646,3 +646,15 @@ $(function() {
 	$(this).parents('table').find('col:eq('+$(this).index()+')').toggleClass('hover');
 	});
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("content").style.marginLeft = "400px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+}
